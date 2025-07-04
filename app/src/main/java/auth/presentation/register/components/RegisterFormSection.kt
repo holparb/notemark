4e5d279559e9e6061569a6e631e3eee9277ccdaf
Balazs Holpar <balazs02.holpar@gmail.com>
2025-07-04 05:48:14 +0200

    Add input secret option to NoteMarkTextInput and create keyboard options and actions for RegisterFormSection

--- app/src/main/java/auth/presentation/register/components/RegisterFormSection.kt
+++ app/src/main/java/auth/presentation/register/components/RegisterFormSection.kt
@@ -4,14 +4,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import auth.presentation.components.TextLink
@@ -55,6 +60,8 @@ fun RegisterFormSection(
         mutableStateOf(false)
     }
 
+    val focusManager = LocalFocusManager.current
+
     Column(
         modifier = modifier.fillMaxWidth()
     ) {
@@ -68,7 +75,15 @@ fun RegisterFormSection(
             isSupportingTextVisible = isUsernameInputFocused,
             modifier = Modifier.onFocusChanged { focusState ->
                 isUsernameInputFocused = focusState.isFocused
-            }
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {
+                    focusManager.moveFocus(FocusDirection.Down)
+                }
+            )
         )
         Spacer(modifier = Modifier.height(16.dp))
         NoteMarkTextInput(
@@ -79,7 +94,15 @@ fun RegisterFormSection(
             isError = !isEmailValid && !isEmalInputFocused,
             modifier = Modifier.onFocusChanged { focusState ->
                 isEmalInputFocused = focusState.isFocused
-            }
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {
+                    focusManager.moveFocus(FocusDirection.Down)
+                }
+            )
         )
         Spacer(modifier = Modifier.height(16.dp))
         NoteMarkTextInput(
@@ -92,7 +115,16 @@ fun RegisterFormSection(
             isSupportingTextVisible = isPasswordInputFocused,
             modifier = Modifier.onFocusChanged { focusState ->  
                 isPasswordInputFocused = focusState.isFocused
-            }
+            },
+            isInputSecret = true,
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {
+                    focusManager.moveFocus(FocusDirection.Down)
+                }
+            )
         )
         Spacer(modifier = Modifier.height(16.dp))
         NoteMarkTextInput(
@@ -103,7 +135,16 @@ fun RegisterFormSection(
             isError = !isRepeatPasswordValid && !isRepeatPasswordInputFocused,
             modifier = Modifier.onFocusChanged { focusState ->
                 isRepeatPasswordInputFocused = focusState.isFocused
-            }
+            },
+            isInputSecret = true,
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    focusManager.clearFocus()
+                }
+            )
         )
         Spacer(modifier = Modifier.height(24.dp))
         PrimaryButton(
--- app/src/main/java/com/holparb/notemark/core/presentation/designsystem/textinputs/NoteMarkTextInput.kt
+++ app/src/main/java/com/holparb/notemark/core/presentation/designsystem/textinputs/NoteMarkTextInput.kt
@@ -8,17 +8,30 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.holparb.notemark.R
+import com.holparb.notemark.core.presentation.designsystem.theme.Eye
+import com.holparb.notemark.core.presentation.designsystem.theme.EyeOff
 import com.holparb.notemark.core.presentation.designsystem.theme.NoteMarkTheme
 
 @Composable
@@ -34,8 +47,13 @@ fun NoteMarkTextInput(
     enabled: Boolean = true,
     keyboardActions: KeyboardActions = KeyboardActions.Default,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
-    singleLine: Boolean = true
+    singleLine: Boolean = true,
+    isInputSecret: Boolean = false,
 ) {
+    var showText by remember {
+        mutableStateOf(false)
+    }
+
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.Start,
@@ -82,7 +100,34 @@ fun NoteMarkTextInput(
             isError = isError,
             keyboardActions = keyboardActions,
             keyboardOptions = keyboardOptions,
-            singleLine = singleLine
+            singleLine = singleLine,
+            trailingIcon = {
+                if(isInputSecret) {
+                    IconButton(
+                        onClick = {
+                            showText = !showText
+                        }
+                    ) {
+                        Icon(
+                            imageVector = if (showText) {
+                                Icons.Outlined.EyeOff
+                            } else {
+                                Icons.Outlined.Eye
+                            },
+                            contentDescription = if (showText) {
+                                stringResource(R.string.hide_password)
+                            } else {
+                                stringResource(R.string.show_password)
+                            }
+                        )
+                    }
+                }
+            },
+            visualTransformation = if(isInputSecret && !showText) {
+                PasswordVisualTransformation('*')
+            } else {
+                VisualTransformation.None
+            }
         )
     }
 }
