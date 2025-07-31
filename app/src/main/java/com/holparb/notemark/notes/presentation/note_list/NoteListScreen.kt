78acdf167d348d9335f92d4b9a2ca0408158f0da
Balazs Holpar <balazs02.holpar@gmail.com>
2025-07-31 06:25:44 +0200

    Create note list top bar

--- app/src/main/java/com/holparb/notemark/notes/presentation/note_list/NoteListScreen.kt
+++ app/src/main/java/com/holparb/notemark/notes/presentation/note_list/NoteListScreen.kt
@@ -1,12 +1,18 @@
 package com.holparb.notemark.notes.presentation.note_list
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.holparb.notemark.core.presentation.designsystem.theme.NoteMarkTheme
+import com.holparb.notemark.notes.presentation.note_list.components.NoteListTopBar
 
 @Composable
 fun NoteListRoot(
@@ -25,7 +31,18 @@ fun NoteListScreen(
     state: NoteListState,
     onAction: (NoteListAction) -> Unit,
 ) {
-    Text("Note list")
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentWindowInsets = WindowInsets.systemBars,
+        topBar = {
+           NoteListTopBar(
+               userInitials = "PL"
+           )
+        }
+    ) { innerPadding ->
+        Text("note list")
+    }
 }
 
 @Preview
