7e19d2e76e538863d49c63af1a50b30f8cdeb559
Balazs Holpar <balazs02.holpar@gmail.com>
2025-08-26 17:05:55 +0200

    Update note creation viewmodel and ui

--- app/src/main/java/com/holparb/notemark/notes/presentation/create_edit_note/CreateEditNoteAction.kt
+++ app/src/main/java/com/holparb/notemark/notes/presentation/create_edit_note/CreateEditNoteAction.kt
@@ -3,4 +3,6 @@ package com.holparb.notemark.notes.presentation.create_edit_note
 sealed interface CreateEditNoteAction {
     data class NoteTitleChanged(val text: String): CreateEditNoteAction
     data class NoteContentChanged(val text: String): CreateEditNoteAction
+    data object CancelClicked: CreateEditNoteAction
+    data object SaveNoteClicked: CreateEditNoteAction
 }
--- app/src/main/java/com/holparb/notemark/notes/presentation/create_edit_note/CreateEditNoteViewModel.kt
+++ app/src/main/java/com/holparb/notemark/notes/presentation/create_edit_note/CreateEditNoteViewModel.kt
@@ -1,21 +1,34 @@
 package com.holparb.notemark.notes.presentation.create_edit_note
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.toRoute
+import com.holparb.notemark.app.navigation.NavigationRoute
+import com.holparb.notemark.notes.domain.repository.NoteRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 
-class CreateEditNoteViewModel : ViewModel() {
+class CreateEditNoteViewModel(
+    private val savedStateHandle: SavedStateHandle,
+    private val noteRepository: NoteRepository
+) : ViewModel() {
 
     private var hasLoadedInitialData = false
+
+    private val route = savedStateHandle.toRoute<NavigationRoute.CreateEditNote>()
+    private val noteId = route.noteId
 
     private val _state = MutableStateFlow(CreateEditNoteState())
     val state = _state
         .onStart {
             if (!hasLoadedInitialData) {
-                /** Load initial data here **/
+                if(noteId.isNotBlank()) {
+                    loadNote(noteId)
+                }
                 hasLoadedInitialData = true
             }
         }
@@ -27,8 +40,34 @@ class CreateEditNoteViewModel : ViewModel() {
 
     fun onAction(action: CreateEditNoteAction) {
         when (action) {
-            else -> TODO("Handle actions")
+            is CreateEditNoteAction.NoteContentChanged -> noteContentChanged(action.text)
+            is CreateEditNoteAction.NoteTitleChanged -> noteTitleChanged(action.text)
+            CreateEditNoteAction.CancelClicked -> cancelNote()
+            CreateEditNoteAction.SaveNoteClicked -> saveNote()
         }
     }
 
+    private fun saveNote() {
+
+    }
+
+    private fun cancelNote() {
+
+    }
+
+    private fun noteTitleChanged(text: String) {
+        _state.update {
+            it.copy(title = text)
+        }
+    }
+
+    private fun noteContentChanged(text: String) {
+        _state.update {
+            it.copy(content = text)
+        }
+    }
+
+    private fun loadNote(noteId: String) {
+
+    }
 }
