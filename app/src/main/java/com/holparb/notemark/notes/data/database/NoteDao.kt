795b982eddaa8db94a6c0ae465c5272f375d2726
Balazs Holpar <balazs02.holpar@gmail.com>
2025-09-29 06:44:22 +0200

    Create sync entity and queries in dao

--- app/src/main/java/com/holparb/notemark/notes/data/database/NoteDao.kt
+++ app/src/main/java/com/holparb/notemark/notes/data/database/NoteDao.kt
@@ -1,7 +1,9 @@
 package com.holparb.notemark.notes.data.database
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Upsert
 import kotlinx.coroutines.flow.Flow
 
@@ -25,4 +27,28 @@ interface NoteDao {
 
     @Query("DELETE FROM notes WHERE noteId = :noteId")
     suspend fun deleteNoteById(noteId: String)
+
+    @Query("DELETE FROM notes")
+    suspend fun clearNotes()
+
+    @Query("SELECT * from sync_queue WHERE userId = :userId")
+    suspend fun getSyncEntriesByUserId(userId: String): List<SyncEntity>
+
+    @Insert
+    suspend fun insertSyncEntry(syncEntry: SyncEntity)
+
+    @Query("DELETE from sync_queue WHERE id = :syncEntryId")
+    suspend fun deleteSyncEntryById(syncEntryId: String)
+
+    @Transaction
+    suspend fun upsertNoteWithSync(note: NoteEntity, syncEntry: SyncEntity) {
+        upsertNote(note)
+        insertSyncEntry(syncEntry)
+    }
+
+    @Transaction
+    suspend fun deleteNoteWithSync(noteId: String, syncEntry: SyncEntity) {
+        deleteNoteById(noteId)
+        insertSyncEntry(syncEntry)
+    }
 }
---
+++ app/src/main/java/com/holparb/notemark/notes/data/database/OperationType.kt
@@ -0,0 +1,5 @@
+package com.holparb.notemark.notes.data.database
+
+enum class OperationType {
+    CREATE, UPDATE, DELETE
+}
